3c06972197c2148c468d89d05882a7a13cc6510c
Jérémy Barré <73218559+JeremyBarre@users.noreply.github.com>
2022-04-19 11:52:51 +0200

    Récupération code

---
+++ Assets/Scripts/FightGobelin/MeleeAttack.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+
+    public float cooldownTime = 2f;
+    public static int noOfClicks = 0;
+    
+    private float nextFireTime = 0f;
+    
+    float lastClickedTime = 0;
+    float maxComboDelay = 1;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void Update()
+    {   
+        
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.8f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        {
+            anim.SetBool("Hit1", false);
+        }
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+        {
+            anim.SetBool("Hit2", false);
+        }
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit3"))
+        {
+            anim.SetBool("Hit3", false);
+            noOfClicks = 0;
+            
+        }
+        if(Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+        if(Time.time > nextFireTime)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Combo();
+            }
+        }
+    }
+    void Combo()
+    {
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if(noOfClicks == 1)
+        {
+            anim.SetBool("Hit1", true);
+        }
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+
+        if(noOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.8f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        {
+            anim.SetBool("Hit1", false);
+            anim.SetBool("Hit2", true);
+        }
+        if(noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+        {
+            anim.SetBool("Hit2", false);
+            anim.SetBool("Hit3", true);
+        }
+    }
+}
